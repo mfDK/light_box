1d99027e78f464943821327f3dc6c66b616350ef
Danny Kim <dkim7474@gmail.com>
2015-12-11 14:52:49 -0500

    added final lightbox feature to the js file

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-	$(".light-link").on('click',function(){
+	$(".light-link").on('click',function(e){
+		e.preventDefault();
+		var $image = $("#chosen-image");
 		$(".outer-lightbox").show();
+		$image.attr("src",$(this).attr("src"));
 		$(".outer-lightbox").on('click',function(){
 			$(this).hide();
 		});
