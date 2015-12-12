142566d897e888cd07cc12b23049a0644e11b79c
Danny Kim <dkim7474@gmail.com>
2015-12-11 21:19:56 -0500

    added text caption functionality

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
 		var $image = $("#chosen-image");
 		$(".outer-lightbox").show();
 		$image.attr("src",$(this).attr("src"));
+		var imageCap = $image.attr("src");
+		if(imageCap == "hulk.jpg"){
+			$("#caption").text("Test");
+		} else if(imageCap == "bengal.jpg"){
+			$("#caption").text("Cat");
+		}
 		$(".outer-lightbox").on('click',function(){
 			$(this).hide();
 		});
